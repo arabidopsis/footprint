08a7e674ff3357ff7ae75e16833ba06269fc14ec
Ian Castleden <ian.castleden@gmail.com>
2022-02-09 11:16:16 +0800

    fixed rsync

--- footprint/rsync.py
+++ footprint/rsync.py
@@ -16,24 +16,16 @@ def mkdir(c: Context, directory: str) -> None:
 
 
 def rsync(src: str, tgt: str, verbose: bool = False) -> None:
-    from fabric import Connection
-
-    assert ":" in src, src
-
-    machine_src, src = src.split(":", 1)
-    if not src.endswith("/"):
-        src += "/"
-    if ":" in tgt:
-        machine_tgt, tgt = tgt.split(":", 1)
-        machine_tgt += ":"
-    else:
-        machine_tgt = ""
 
     v = "-v" if verbose else ""
-    with Connection(machine_src) as c:
-        c.run(f"test -d {src}")
-        cmd = f"""rsync -a {v} --delete {src} {machine_tgt}{tgt}"""
-        c.run(cmd)
+    c = Context()
+
+    if not src.endswith("/"):
+        src += "/"
+    if tgt.endswith("/"):
+        tgt = tgt[:-1]
+    cmd = f"""rsync -a {v} --delete {src} {tgt}"""
+    c.run(cmd)
 
 
 @cli.command(name="rsync")
@@ -41,7 +33,5 @@ def rsync(src: str, tgt: str, verbose: bool = False) -> None:
 @click.argument("src")
 @click.argument("tgt")
 def rsync_(src: str, tgt: str, verbose: bool):
-    """Sync two directories on two different machines."""
-    if ":" not in src:
-        raise click.BadParameter("SRC must be {machine}:{directory}", param_hint="src")
+    """Sync two directories on two possibly different machines."""
     rsync(src, tgt, verbose)
