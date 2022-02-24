35fea127d24e3a7f9cf82d633bc830eb9d813295
Ian Castleden <ian.castleden@gmail.com>
2022-02-24 16:51:35 +0800

    poetry requirements

--- footprint/cli.py
+++ footprint/cli.py
@@ -1,5 +1,6 @@
 import click
 from click_didyoumean import DYMGroup
+from flask import request_finished
 
 from .config import VERSION
 
@@ -21,3 +22,23 @@ def update():
 
     cmd = f"{sys.executable} -m pip install -U '{REPO}'"
     Context().run(cmd)
+
+
+@cli.command()
+@click.argument("project_dir", required=False)
+def poetry_to_reqs(project_dir):
+    """Generate a requirements file from pyproject.toml"""
+    import os
+    import toml
+
+    pyproject = "pyproject.toml"
+    if project_dir:
+        pyproject = os.path.join(project_dir, pyproject)
+    if not os.path.isfile(pyproject):
+        raise click.BadArgumentUsage("no pyproject.toml file!")
+
+    reqs = "\n".join(
+        f"{k}{v}"
+        for k, v in toml.load(pyproject)["tool"]["poetry"]["dependencies"].items()
+    )
+    click.echo(reqs)
